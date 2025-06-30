f4afba07433c26d78dc60319fae75c7cf1f1cd7c
nohsn <minuee@kormedi.com>
2025-06-30 20:21:42 +0900

    Update ChatMeMessage.tsx

--- src/components/msgType/ChatMeMessage.tsx
+++ src/components/msgType/ChatMeMessage.tsx
@@ -1,5 +1,5 @@
 import { Box,Flex,Text,useColorModeValue ,Icon} from '@chakra-ui/react';
-
+import { BrowserView,isMobileOnly,isBrowser,isDesktop,isMobile} from "react-device-detect";
 type ChatMeMessageProps = {
   question: string;
     indexKey : any
@@ -16,7 +16,7 @@ const ChatMeMessage = ({  question = "대답이 중지되었습니다.", indexKe
   const textSystemStopIconColor = useColorModeValue('#5E0018', 'navy.800');
   
   return (
-    <Flex w="100%" align={'center'} mb="28px"  mt={indexKey === 0 ? 0 : "28px"}  justifyContent='flex-end'>
+    <Flex w="100%" align={'center'} mb="28px"  mt={ indexKey === 0 ? isMobileOnly ? "16px" : 0 : "28px"}  justifyContent='flex-end'>
       <Flex 
         padding="12px 20px" 
         border={`1px solid ${bgMeColor}`} 
